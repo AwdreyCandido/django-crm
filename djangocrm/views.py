1b5526c6e58404a53492d4909eca1c55cfd9c31d
Awdrey Candido <awdrey.dev@gmail.com>
2023-09-16 18:29:32 -0300

    feat: configuring login

--- djangocrm/views.py
+++ djangocrm/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
+    
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            message.success(request, "You have been logged in")
+            return redirect('home')
+
     return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
